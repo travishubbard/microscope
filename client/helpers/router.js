68ac40a6b74d853cc079f27ca320f5d4fae48347
Travis Hubbard <travis.hubbard@gmail.com>
2013-08-22 19:35:35 -0400

    deny access to new posts page when not logged in

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -8,3 +8,14 @@ Meteor.Router.add({
 
 	'/submit': 'postSubmit'
 });
+
+Meteor.Router.filters({
+	'requireLogin': function(page){
+		if(meteor.user())
+			return page;
+		else
+			return 'accessDenied';
+	}
+});
+
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
