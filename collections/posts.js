c9765e8c47dc6ce15e925a30c32b256b5188dca9
Travis Hubbard <travis.hubbard@gmail.com>
2013-08-23 21:22:58 -0400

    fixed Meteor.Error call

--- collections/posts.js
+++ collections/posts.js
@@ -1,6 +1,5 @@
 Posts = new Meteor.Collection('posts');
 
-
 Meteor.methods({
 	post: function(postAttributes){
 		var user = Meteor.user(),
@@ -8,18 +7,15 @@ Meteor.methods({
 
 		//ensure the user is logged in
 		if (!user)
-			throw new Meteor.Errror(401, "You need to login to post new stories");
+			throw new Meteor.Error(401, "You need to login to post new stories");
 
 		//ensure the post has a title
 		if (!postAttributes.title)
-			throw new Meteor.Errror(422, 'Please fill in a headline');
+			throw new Meteor.Error(422, 'Please fill in a headline');
 
 		// check that there are no previous posts with the same link
-		if (postAttributes.url && postWithSameLink) {
-			throw new Meteor.Errror(302, 
-				'This link has already been posted',
-				postWithSameLink._id);
-		}
+		if (postAttributes.url && postWithSameLink)
+			throw new Meteor.Error(302, 'This link has already been posted', postWithSameLink._id);
 
 		//pick out the whitelisted keys
 		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
