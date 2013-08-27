886f9609756f233011885041d002a11c96509e62
Travis Hubbard <travis.hubbard@gmail.com>
2013-08-27 12:39:08 -0400

    denormalized the number of comments into the post

--- server/fixtures.js
+++ server/fixtures.js
@@ -20,7 +20,8 @@ if (Posts.find().count() === 0) {
 		title: 'Introducing Telescope',
 		userId: sacha._id,
 		author: sacha.profile.name,
-		url: 'http://sachagreif.com/introducing-telescope/', submitted: now - 7 * 3600 * 1000
+		url: 'http://sachagreif.com/introducing-telescope/', submitted: now - 7 * 3600 * 1000,
+		commentsCount: 2
 	});
 	
 	Comments.insert({
@@ -41,13 +42,15 @@ if (Posts.find().count() === 0) {
 		title: 'Meteor',
 		userId: tom._id,
 		author: tom.profile.name,
-		url: 'http://meteor.com', submitted: now - 10 * 3600 * 1000
+		url: 'http://meteor.com', submitted: now - 10 * 3600 * 1000,
+		commentsCount: 0
 	});
 
 	Posts.insert({
 		title: 'The Meteor Book',
 		userId: tom._id,
 		author: tom.profile.name,
-		url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000
+		url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000,
+		commentsCount: 0
 	});
 }
